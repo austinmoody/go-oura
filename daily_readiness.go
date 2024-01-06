112bf48ad0f7585a0071baf6d7788ec2f4b357bc
Austin Moody <austin.moody@gmail.com>
2024-01-05 21:11:11 -0500

    Create date.go for date conversion functionality
    Moved the date conversion functionality from daily_readiness.go to a new file, date.go. This has been done to make code more modular and enhance readability. The date.go file now has the logic for converting the API return dates from string to time.Time.

--- daily_readiness.go
+++ daily_readiness.go
@@ -4,28 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/url"
-	"strings"
 	"time"
 )
 
 /*
 https://cloud.ouraring.com/v2/docs#tag/Daily-Readiness-Routes
 */
-
-type Date struct {
-	time.Time
-}
-
-func (d *Date) UnmarshalJSON(input []byte) error {
-	strInput := strings.Trim(string(input), `"`)
-	newTime, err := time.Parse("2006-01-02", strInput)
-	if err != nil {
-		return err
-	}
-
-	d.Time = newTime
-	return nil
-}
 
 type DailyReadinessDocuments struct {
 	Documents []DailyReadinessDocument `json:"data"`
---
+++ date.go
@@ -0,0 +1,25 @@
+package go_oura
+
+/*
+	Simply here to convert YYYY-MM-DD dates in API returns from string to time.Time
+*/
+
+import (
+	"strings"
+	"time"
+)
+
+type Date struct {
+	time.Time
+}
+
+func (d *Date) UnmarshalJSON(input []byte) error {
+	strInput := strings.Trim(string(input), `"`)
+	newTime, err := time.Parse("2006-01-02", strInput)
+	if err != nil {
+		return err
+	}
+
+	d.Time = newTime
+	return nil
+}
