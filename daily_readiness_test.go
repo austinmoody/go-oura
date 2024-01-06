9db4db2c9731fcf3508e1cd9612b742159246ad3
Austin Moody <austin.moody@gmail.com>
2024-01-06 17:01:06 -0500

    Remove timestamp related files and refactor DailyReadinessDocument
    The `timestamp.go` and `timestamp_test.go` files have been removed. The `DailyReadinessDocument` in `daily_readiness.go` file has been refactored for better code structuring and error reporting. Required field checks are now implemented during unmarshalling of JSON data into a `DailyReadinessDocument` instance. Also, added the `daily_readiness_test.go` tests file to ensure that the new logic functions correctly.

---
+++ daily_readiness_test.go
@@ -0,0 +1,90 @@
+package go_oura
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestGetReadinessDocument(t *testing.T) {
+	tt := []struct {
+		name           string
+		documentId     string
+		mockResponse   string
+		expectedOutput DailyReadinessDocument
+		expectErr      bool
+	}{
+		{
+			name:         "ValidResponse",
+			documentId:   "1",
+			mockResponse: `{"id":"29a809a2-778c-4742-b945-e01876b8f32a","contributors":{"activity_balance":86,"body_temperature":89,"hrv_balance":4,"previous_day_activity":88,"previous_night":87,"recovery_index":99,"resting_heart_rate":1,"sleep_balance":90},"day":"2024-01-01","score":63,"temperature_deviation":0.2,"temperature_trend_deviation":0.38,"timestamp":"2024-01-01T00:00:00+00:00"}`,
+			expectedOutput: DailyReadinessDocument{
+				Id: "29a809a2-778c-4742-b945-e01876b8f32a",
+				Day: func() Date {
+					layout := "2006-01-02"
+					t, _ := time.Parse(layout, "2024-01-01")
+					return Date{Time: t}
+				}(),
+				Score:                     63,
+				TemperatureDeviation:      0.2,
+				TemperatureTrendDeviation: 0.38,
+				Timestamp: func() time.Time {
+					layout := "2006-01-02T15:04:05Z07:00"
+					t, _ := time.Parse(layout, "2024-01-01T00:00:00+00:00")
+					return t
+				}(),
+
+				Contributors: Contributors{
+					ActivityBalance:     86,
+					BodyTemperature:     89,
+					HrvBalance:          4,
+					PreviousDayActivity: 88,
+					PreviousNight:       87,
+					RecoveryIndex:       99,
+					RestingHeartRate:    1,
+					SleepBalance:        90,
+				},
+			},
+			expectErr: false,
+		},
+		{
+			name:           "Invalid Response",
+			documentId:     "2",
+			mockResponse:   `{"message": "invalid"}`,
+			expectedOutput: DailyReadinessDocument{},
+			expectErr:      true,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+				rw.Write([]byte(tc.mockResponse))
+			}))
+
+			config := ClientConfig{
+				BaseUrl:    server.URL,
+				HTTPClient: server.Client(),
+			}
+
+			client := Client{config: config}
+
+			activity, err := client.GetReadinessDocument(tc.documentId)
+			if tc.expectErr {
+				if err == nil {
+					t.Errorf("Expected error, got nil")
+				}
+				return
+			} else if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+				return
+			}
+
+			if !reflect.DeepEqual(activity, tc.expectedOutput) {
+				t.Errorf("Expected %v, got %v", tc.expectedOutput, activity)
+			}
+		})
+	}
+}
