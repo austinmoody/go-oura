c84bd4bc5221db978c837f1ce2001297ef398602
Austin Moody <austin.moody@gmail.com>
2024-01-07 21:03:33 -0500

    Add UnmarshalJSON method for daily readiness documents
    This update adds a custom UnmarshalJSON method for the DailyReadinessDocuments type, ensuring that all required fields are present in the JSON document. Also, related tests for checking the correctness of the added function are introduced. The update improves the error handling of JSON parsing, providing more detailed feedback when required fields are missing.

--- tests/daily_readiness_test.go
+++ tests/daily_readiness_test.go
@@ -9,6 +9,93 @@ import (
 	"time"
 )
 
+func TestGetReadinessDocuments(t *testing.T) {
+	tt := []struct {
+		name           string
+		startTime      time.Time
+		endTime        time.Time
+		mockResponse   string
+		expectedOutput go_oura.DailyReadinessDocuments
+		expectErr      bool
+	}{
+		{
+			name:         "ValidResponse",
+			startTime:    time.Now().Add(-1 * time.Hour),
+			endTime:      time.Now().Add(-2 * time.Hour),
+			mockResponse: `{"data":[{"id":"29a809a2-778c-4742-b945-e01876b8f32a","contributors":{"activity_balance":86,"body_temperature":89,"hrv_balance":4,"previous_day_activity":88,"previous_night":87,"recovery_index":99,"resting_heart_rate":1,"sleep_balance":90},"day":"2024-01-01","score":63,"temperature_deviation":0.2,"temperature_trend_deviation":0.38,"timestamp":"2024-01-01T00:00:00+00:00"}],"next_token":null}`,
+			expectedOutput: go_oura.DailyReadinessDocuments{
+				Documents: []go_oura.DailyReadinessDocument{
+					{
+						Id: "29a809a2-778c-4742-b945-e01876b8f32a",
+						Day: func() go_oura.Date {
+							layout := "2006-01-02"
+							t, _ := time.Parse(layout, "2024-01-01")
+							return go_oura.Date{Time: t}
+						}(),
+						Score:                     63,
+						TemperatureDeviation:      0.2,
+						TemperatureTrendDeviation: 0.38,
+						Timestamp: func() time.Time {
+							layout := "2006-01-02T15:04:05Z07:00"
+							t, _ := time.Parse(layout, "2024-01-01T00:00:00+00:00")
+							return t
+						}(),
+
+						Contributors: go_oura.Contributors{
+							ActivityBalance:     86,
+							BodyTemperature:     89,
+							HrvBalance:          4,
+							PreviousDayActivity: 88,
+							PreviousNight:       87,
+							RecoveryIndex:       99,
+							RestingHeartRate:    1,
+							SleepBalance:        90,
+						}},
+				},
+			},
+			expectErr: false,
+		},
+		{
+			name:           "InvalidResponse",
+			startTime:      time.Now().Add(-3 * time.Hour),
+			endTime:        time.Now().Add(-4 * time.Hour),
+			mockResponse:   `{"message": "invalid"}`,
+			expectedOutput: go_oura.DailyReadinessDocuments{},
+			expectErr:      true,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+				_, err := rw.Write([]byte(tc.mockResponse))
+				if err != nil {
+					http.Error(rw, err.Error(), http.StatusInternalServerError)
+					return
+				}
+			}))
+
+			client := go_oura.NewClientWithUrlAndHttp("", server.URL, server.Client())
+
+			activity, err := client.GetReadinessDocuments(tc.startTime, tc.endTime)
+			if tc.expectErr {
+				if err == nil {
+					t.Errorf("Expected error, got nil")
+				}
+				return
+			} else if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+				return
+			}
+
+			if !reflect.DeepEqual(activity, tc.expectedOutput) {
+				t.Errorf("Expected %v, got %v", tc.expectedOutput, activity)
+			}
+		})
+	}
+
+}
+
 func TestGetReadinessDocument(t *testing.T) {
 	tt := []struct {
 		name           string
