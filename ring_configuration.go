2524d4bf3878a22ab680a83f89505d4061b1a9c6
Austin Moody <austin.moody@gmail.com>
2024-01-23 17:10:03 -0500

    Update ring_configuration.go with documentation.

--- ring_configuration.go
+++ ring_configuration.go
@@ -1,3 +1,8 @@
+// Package go_oura provides a simple binding to the Oura Ring v2 API
+
+// This file contains code related to ring configurations, basically information about your rings
+// Ring Configuration API https://cloud.ouraring.com/v2/docs#tag/Ring-Configuration-Routes
+
 package go_oura
 
 import (
@@ -8,11 +13,15 @@ import (
 	"time"
 )
 
+// RingConfigurations stores a list of ring configuration items along with a token which may be used to pull the next batch of RingConfiguration items from the API.
+// JSON described at https://cloud.ouraring.com/v2/docs#operation/Multiple_ring_configuration_Documents_v2_usercollection_ring_configuration_get
 type RingConfigurations struct {
 	Items     []RingConfiguration `json:"data"`
 	NextToken string              `json:"next_token"`
 }
 
+// RingConfiguration stores specifics for a single Ring's configuration
+// JSON described at https://cloud.ouraring.com/v2/docs#operation/Single_ring_configuration_Document_v2_usercollection_ring_configuration__document_id__get
 type RingConfiguration struct {
 	ID              string     `json:"id"`
 	Color           string     `json:"color"`
@@ -26,6 +35,7 @@ type RingConfiguration struct {
 type ringConfigurationBase RingConfiguration
 type ringConfigurationsBase RingConfigurations
 
+// UnmarshalJSON is a helper function to convert a ring configuration JSON from the API to the RingConfiguration type.
 func (rc *RingConfiguration) UnmarshalJSON(data []byte) error {
 	if err := checkJSONFields(reflect.TypeOf(*rc), data); err != nil {
 		return err
@@ -41,6 +51,7 @@ func (rc *RingConfiguration) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
+// UnmarshalJSON is a helper function to convert a list of ring configurations JSON from the API to the RingConfigurations type.
 func (rc *RingConfigurations) UnmarshalJSON(data []byte) error {
 	if err := checkJSONFields(reflect.TypeOf(*rc), data); err != nil {
 		return err
@@ -56,6 +67,9 @@ func (rc *RingConfigurations) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
+// GetRingConfigurations accepts a start & end date and returns a RingConfigurations object which will contain any RingConfiguration
+// found in the time period.  Optionally the next token can be passed which tells the API to give the next set of
+// ring configurations if the date range returns a large set.
 func (c *Client) GetRingConfigurations(startDate time.Time, endDate time.Time, nextToken *string) (RingConfigurations, *OuraError) {
 	urlParameters := url.Values{
 		"start_date": []string{startDate.Format("2006-01-02")},
@@ -89,10 +103,11 @@ func (c *Client) GetRingConfigurations(startDate time.Time, endDate time.Time, n
 	return ringConfigurations, nil
 }
 
-func (c *Client) GetRingConfiguration(documentId string) (RingConfiguration, *OuraError) {
+// GetRingConfiguration calls the Oura Ring API with a specific Ring Configuration identifer and returns a RingConfiguration object
+func (c *Client) GetRingConfiguration(ringConfigurationId string) (RingConfiguration, *OuraError) {
 
 	apiResponse, ouraError := c.Getter(
-		fmt.Sprintf(RingConfigurationUrl+"/%s", documentId),
+		fmt.Sprintf(RingConfigurationUrl+"/%s", ringConfigurationId),
 		nil,
 	)
 
