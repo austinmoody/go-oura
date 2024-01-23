2d996335713557506537da77e5a0ab5e629421d0
Austin Moody <austin.moody@gmail.com>
2024-01-22 21:46:36 -0500

    Add documentation to heartrates.go

--- heartrates.go
+++ heartrates.go
@@ -1,3 +1,8 @@
+// Package go_oura provides a simple binding to the Oura Ring v2 API
+
+// This file contains code related to heart rates recorded by the Oura Ring
+// Heart Rate API description: https://cloud.ouraring.com/v2/docs#tag/Heart-Rate-Routes
+
 package go_oura
 
 import (
@@ -8,11 +13,14 @@ import (
 	"time"
 )
 
+// HeartRates stores a list of heart rate items along with a token which may be used to pull the next batch of HeartRate items from the API.
+// JSON described at https://cloud.ouraring.com/v2/docs#operation/Multiple_Heart_Rate_Documents_v2_usercollection_heartrate_get
 type HeartRates struct {
 	Items     []HeartRate `json:"data"`
 	NextToken string      `json:"next_token"`
 }
 
+// HeartRate stores specifics for a recorded heart rate by the Oura Ring
 type HeartRate struct {
 	Bpm       int       `json:"bpm"`
 	Source    string    `json:"source"`
@@ -22,6 +30,7 @@ type HeartRate struct {
 type heartRatesBase HeartRates
 type hearRateBase HeartRate
 
+// UnmarshalJSON is a helper function to convert a heart rate JSON from the API to the HeartRate type.
 func (hr *HeartRate) UnmarshalJSON(data []byte) error {
 	if err := checkJSONFields(reflect.TypeOf(*hr), data); err != nil {
 		return err
@@ -37,6 +46,7 @@ func (hr *HeartRate) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
+// UnmarshalJSON is a helper function to convert a list of heart rate JSON from the API to the HeartRates type.
 func (hr *HeartRates) UnmarshalJSON(data []byte) error {
 	if err := checkJSONFields(reflect.TypeOf(*hr), data); err != nil {
 		return err
@@ -53,6 +63,9 @@ func (hr *HeartRates) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
+// GetHeartRates accepts a start & end date and returns a HeartRates object which will contain any HeartRate
+// found in the time period.  Optionally the next token can be passed which tells the API to give the next set of
+// heart rates if the date range returns a large set.
 func (c *Client) GetHeartRates(startDateTime time.Time, endDateTime time.Time, nextToken *string) (HeartRates, *OuraError) {
 
 	urlParameters := url.Values{
