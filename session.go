e5e4bbbf734e107cc5f4e38735fb81df3b940095
Austin Moody <austin.moody@gmail.com>
2024-01-23 21:29:40 -0500

    Add documentation to session.go

--- session.go
+++ session.go
@@ -1,3 +1,8 @@
+// Package go_oura provides a simple binding to the Oura Ring v2 API
+
+// This file contains code related to sessions recorded by an Oura Ring
+// Session API https://cloud.ouraring.com/v2/docs#tag/Session-Routes
+
 package go_oura
 
 import (
@@ -8,11 +13,15 @@ import (
 	"time"
 )
 
+// Sessions stores a list of session items along with a token which may be used to pull the next batch of Session items from the API.
+// JSON described at https://cloud.ouraring.com/v2/docs#operation/Multiple_session_Documents_v2_usercollection_session_get
 type Sessions struct {
 	Items      []Session `json:"data"`
 	Next_Token string    `json:"next_token"`
 }
 
+// Session stores specifics for a single session recorded by an Oura Ring
+// JSON described at https://cloud.ouraring.com/v2/docs#operation/Single_session_Document_v2_usercollection_session__document_id__get
 type Session struct {
 	ID                       string           `json:"id"`
 	Day                      Date             `json:"day"`
@@ -34,6 +43,7 @@ type SessionDataItems struct {
 type sessionBase Session
 type sessionsBase Sessions
 
+// UnmarshalJSON is a helper function to convert a recorded session JSON from the API to the Session type.
 func (s *Session) UnmarshalJSON(data []byte) error {
 	if err := checkJSONFields(reflect.TypeOf(*s), data); err != nil {
 		return err
@@ -49,6 +59,7 @@ func (s *Session) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
+// UnmarshalJSON is a helper function to convert multiple recorded sessions JSON from the API to the Sessions type.
 func (s *Sessions) UnmarshalJSON(data []byte) error {
 	if err := checkJSONFields(reflect.TypeOf(*s), data); err != nil {
 		return err
@@ -65,10 +76,11 @@ func (s *Sessions) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
-func (c *Client) GetSession(documentId string) (Session, *OuraError) {
+// GetSession calls the Oura Ring API with a specific session identifer and returns a Session object
+func (c *Client) GetSession(sessionId string) (Session, *OuraError) {
 
 	apiResponse, ouraError := c.Getter(
-		fmt.Sprintf(SessionUrl+"/%s", documentId),
+		fmt.Sprintf(SessionUrl+"/%s", sessionId),
 		nil,
 	)
 
@@ -90,6 +102,9 @@ func (c *Client) GetSession(documentId string) (Session, *OuraError) {
 	return session, nil
 }
 
+// GetSessions accepts a start & end date and returns a Sessions object which will contain any Session
+// found in the time period.  Optionally the next token can be passed which tells the API to give the next set of
+// sessions if the date range returns a large set.
 func (c *Client) GetSessions(startDate time.Time, endDate time.Time, nextToken *string) (Sessions, *OuraError) {
 
 	urlParameters := url.Values{
